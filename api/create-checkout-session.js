ed870ba0afd424c48d1e3781a4d5d877410f1bfa
andreacepeda <andreacepeda888@gmail.com>
2026-07-26 13:41:03 -0500

    fixing stripe def not done

--- api/create-checkout-session.js
+++ api/create-checkout-session.js
@@ -24,6 +24,7 @@ export default async function handler(req, res) {
       line_items: [{ price: priceId, quantity: qty }],
       success_url: `${origin}/?checkout=success`,
       cancel_url: `${origin}/?checkout=canceled`,
+      managed_payments: { enabled: false },
     });
 
     return res.status(200).json({ url: session.url });
